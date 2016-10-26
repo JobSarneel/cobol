4adefd25385bee8a675c8480c5bb57cc7b90869f
JobSarneel <j.sarneel@gmail.com>
2016-10-26 07:21:05 +0200

    ideTest

--- ideTest.cob
+++ ideTest.cob
@@ -11,7 +11,7 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
        01  n   PIC 99      VALUE 2.
-       01  cq  PIC 99.
+       01  cq  PIC X.
 
        PROCEDURE DIVISION.
        Begin.
@@ -19,18 +19,24 @@
            IF n > 1
              DISPLAY "Let's troll :-D"
            END-IF
+
+           PERFORM ContinueOrQuit
+           DISPLAY "Ok, so you want to continue? Too bad, this is all "
+                   "there is.."
            STOP RUN.
 
        ContinueOrQuit.
-           DISPLAY "Press 'c' to continue entering another book or 'q'"
+           DISPLAY "Press 'c' to continue or 'q'"
                    " to quit: " WITH NO ADVANCING
            ACCEPT cq
            EVALUATE cq
              WHEN "c" DISPLAY " "
-             WHEN "v" DISPLAY "OK, I'll show you your records now."
+             WHEN "q"
+               DISPLAY "OK, we'll stop the program now."
+               STOP RUN
              WHEN OTHER
-           DISPLAY "That's not a 'c' or a 'q'. Try again."
-           PERFORM ContinueOrQuit
+               DISPLAY "That's not a 'c' or a 'q'. Try again."
+               PERFORM ContinueOrQuit
            END-EVALUATE.
 
        END PROGRAM YOUR-PROGRAM-NAME.
