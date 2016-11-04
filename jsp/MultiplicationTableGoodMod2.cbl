fcedac771369e9c910fd8800ad011de599a0d8d5
JobSarneel <j.sarneel@gmail.com>
2016-11-04 18:34:08 +0100

    Three modifications MultiplicationTableGood

---
+++ jsp/MultiplicationTableGoodMod2.cbl
@@ -0,0 +1,32 @@
+      ******************************************************************
+      * Author: M.A. Jackson
+      * Date: seventies
+      * Purpose: example of good program structure
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MultiplicationTableBad.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77  lineNo                  PIC 99.
+       77  colNo                   PIC 99.
+       01  printLine.
+           02 num      OCCURS 10   PIC ZZZ9.
+
+       PROCEDURE DIVISION.
+       PTable.
+           PERFORM PLine VARYING lineNo FROM 10 BY -1
+                   UNTIL lineNo < 1
+           STOP RUN.
+
+       PLine.
+           MOVE SPACES TO printLine
+           PERFORM PNum VARYING colNo FROM 1 BY 1
+                   UNTIL colNo > 11 - lineNo
+           DISPLAY printLine.
+
+       PNum.
+           MULTIPLY lineNo BY colNo GIVING num (colNo).
+
+       END PROGRAM MultiplicationTableBad.
---
+++ jsp/MultiplicationTableGoodMod3.cbl
@@ -0,0 +1,32 @@
+      ******************************************************************
+      * Author: M.A. Jackson
+      * Date: seventies
+      * Purpose: example of good program structure
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MultiplicationTableBad.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77  lineNo                  PIC 99.
+       77  colNo                   PIC 99.
+       01  printLine.
+           02 num      OCCURS 10   PIC ZZZ9.
+
+       PROCEDURE DIVISION.
+       PTable.
+           PERFORM PLine VARYING lineNo FROM 1 BY 1
+                   UNTIL lineNo > 10
+           STOP RUN.
+
+       PLine.
+           MOVE SPACES TO printLine
+           PERFORM PNum VARYING colNo FROM 11 BY 1
+                   UNTIL colNo > 20
+           DISPLAY printLine.
+
+       PNum.
+           MULTIPLY lineNo BY colNo GIVING num (colNo - 10).
+
+       END PROGRAM MultiplicationTableBad.
